646c33726bf74ade05669057ee43e455edc394e4
unknown <rizalahmaddafa24@gmail.com>
2020-08-13 20:40:10 +0700

    migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class AuthController extends Controller
-{
-    public function me(){
-    	return "semoga berhasi;";
+class AuthController extends Controller {
+    
+    public function me() {
+        return [
+            "nis" => 3103118137,
+            "name" => "Wahyudi",
+            "gender" => "Laki-laki",
+            "phone" => 6281575404482,
+            "class" => "XII RPL 4"
+        ];
     }
 }
